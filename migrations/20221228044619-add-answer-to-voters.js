0b3e6e8d37b9510a8733ef1825e7c723d5156fc3
saimall <rameshwaramsaimallik@gmail.com>
2022-12-28 10:37:40 +0530

    add required tables to store response of voter with refernces to unique questionid

---
+++ migrations/20221228044619-add-answer-to-voters.js
@@ -0,0 +1,74 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn("answers", "VoterID", {
+      type: Sequelize.DataTypes.INTEGER,
+    });
+    await queryInterface.addConstraint("answers", {
+      fields: ["VoterID"],
+      type: "foreign key",
+      references: {
+        table: "Voters",
+        field: "id",
+      },
+    });
+
+    await queryInterface.addColumn("answers", "ElectionID", {
+      type: Sequelize.DataTypes.INTEGER,
+    });
+    await queryInterface.addConstraint("answers", {
+      fields: ["ElectionID"],
+      type: "foreign key",
+      references: {
+        table: "Elections",
+        field: "id",
+      },
+    });
+
+    await queryInterface.addColumn("answers", "QuestionID", {
+      type: Sequelize.DataTypes.INTEGER,
+    });
+    await queryInterface.addConstraint("answers", {
+      fields: ["QuestionID"],
+      type: "foreign key",
+      references: {
+        table: "questions",
+        field: "id",
+      },
+    });
+
+    await queryInterface.addColumn("answers", "chossedoption", {
+      type: Sequelize.DataTypes.INTEGER,
+    });
+    await queryInterface.addConstraint("answers", {
+      fields: ["chossedoption"],
+      type: "foreign key",
+      references: {
+        table: "options",
+        field: "id",
+      },
+    });
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+
+    await queryInterface.removeColumn("answers", "VoterID");
+    await queryInterface.removeColumn("answers", "ElectionID");
+    await queryInterface.removeColumn("answers", "QuestionID");
+    await queryInterface.removeColumn("answers", "chossedoption");
+  },
+};
---
+++ models/answers.js
@@ -0,0 +1,44 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class answers extends Model {
+    static addResponse({ VoterID, ElectionID, QuestionID, chossedoption }) {
+      return this.create({
+        VoterID,
+        ElectionID,
+        QuestionID,
+        chossedoption,
+      });
+    }
+
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      answers.belongsTo(models.Voters, {
+        foreignKey: "VoterID",
+      });
+      answers.belongsTo(models.Election, {
+        foreignKey: "ElectionID",
+      });
+      answers.belongsTo(models.questions, {
+        foreignKey: "QuestionID",
+      });
+      answers.belongsTo(models.options, {
+        foreignKey: "chossedoption",
+      });
+
+      // define association here
+    }
+  }
+  answers.init(
+    {},
+    {
+      sequelize,
+      modelName: "answers",
+    }
+  );
+  return answers;
+};
