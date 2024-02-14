9d85e7b478f2f238f061de71c13d04837202da46
Saimall <rameshwaramsaimallik@gmail.com>
2024-02-15 01:11:27 +0530

    Added cypress integration test

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents() {
+      // implement node event listeners here
+    },
+  },
+});
---
+++ cypress/e2e/test.cy.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-undef */
+describe("Online election test suite", () => {
+  const baseUrl = "http://localhost:7000";
+  beforeEach(() => {
+    cy.visit(`${baseUrl}`);
+  });
+
+  it("testing signup new user", () => {
+    // Perform signup actions using Cypress commands
+    cy.visit(`${baseUrl}/signup`);
+    // Fill in signup form fields
+    cy.get("#firstName").type("mallik");
+    cy.get("#lastName").type("sai");
+    cy.get("#email").type("1@test.com");
+    cy.get("#password").type("12345678");
+    // Submit the form
+    cy.get("form").submit();
+  });
+
+  it("testing user login", () => {
+    cy.visit(`${baseUrl}/login`);
+    // Fill in login form fields
+    cy.get("#email").type("sai@test.com");
+    cy.get("#password").type("12345678");
+    // Submit the form
+    cy.get("form").submit();
+  });
+
+  it("testing election creation", () => {
+    cy.visit(`${baseUrl}/login`);
+    // Fill in login form fields
+    cy.get("#email").type("sai@test.com");
+    cy.get("#password").type("12345678");
+    // Submit the form
+    cy.get("form").submit();
+    cy.visit(`${baseUrl}/create`);
+    // Fill in login form fields
+    cy.get("#electionName").type("election1");
+    cy.get("#publicurl").type("newurl");
+    // Submit the form
+    cy.get("form").submit();
+  });
+
+  it("should sign the user out", () => {
+    // Assuming there's a button/link with id "signout"
+    cy.visit(`${baseUrl}/elections`);
+    cy.visit(`${baseUrl}/signout`);
+  });
+});
