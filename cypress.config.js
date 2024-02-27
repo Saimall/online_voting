93dcd683a641a9d6cc6ee8bc0df8616317f46039
Saimall <rameshwaramsaimallik@gmail.com>
2024-02-27 20:35:50 +0530

    Updated sattus messages

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents() {
-      // implement node event listeners here
+      // implement node event listeners
     },
   },
 });
